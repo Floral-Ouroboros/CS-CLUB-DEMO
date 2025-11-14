f75b4b60922899d5e7083d98a41085b411dbbcb2
Floral-Ouroboros <caidenbergeron48@outlook.com>
2025-11-14 16:30:53 -0600

    It spouts no falsehood. Doug is THE GOAT. -CAIDEN

--- Adam.cpp
+++ Adam.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    
+    for (int i = 0; i < 10; ++i) {
+        std::cout << "Hello, World!" << std::endl;
+
+        for (int ii = 0; ii < 10; ii++) {
+            std::cout << "Doug is THE GOAT!!!" << std::endl;
+        }
+
+    }
+
+
+
     return 0;
 }
--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # CS-CLUB-DEMO
 This is a demonstration repository for the Computer Science Club. 
-Test
